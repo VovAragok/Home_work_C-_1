964478e5697f0004cbc34d58c5d59717c5498963
vladimir Rom <vovgen9572@gmail.com>
2023-04-10 19:25:23 +0300

    выполнена 3,4 задача

---
+++ Programm_3/Program_3.cs
@@ -0,0 +1,16 @@
+﻿//3. Напишите программу, которая на вход принимает число и выдаёт, ( является ли число чётным)
+Console.Write("введите число: ");
+string s = Console.ReadLine();
+int i;
+if (int.TryParse(s, out i)){
+     if (i % 2 == 0) {
+        System.Console.WriteLine($"число {i} - чётное");
+        }
+    else {
+        System.Console.WriteLine($"число {i} - нечетное");
+        }
+}
+else{ System.Console.WriteLine("упс");}
+
+
+
---
+++ Programm_4/Program_4.cs
@@ -0,0 +1,11 @@
+﻿// 4. Напишите программу, которая на вход принимает число N и выдаёт все чётные числа от 1 до N 
+Console.Write("введите число: ");
+string s = Console.ReadLine();
+int n;
+if (int.TryParse(s, out n)){
+    for (int i = 1; i <= n; i++){
+        if( i %2 ==0){
+            System.Console.WriteLine(i);
+}}}
+
+else{ System.Console.WriteLine("упс");}
